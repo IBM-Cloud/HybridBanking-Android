d905c54c91690642162784e49b609ed661548048
Ram Vennam <rvennam@Rams-MacBook-Pro.local>
2015-11-11 09:43:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# Hybrid Banking Demo
+
+https://www.youtube.com/watch?v=7OLkIGoBpi0
+
+### Android Application Setup
+This portion will set up the Android application the Banking customer will use to review their loyalty points and submit feedback.
+
+1. Download https://github.com/IBM-Bluemix/HybridBanking-Android/archive/master.zip
+2. Extract it.
+3. Launch Android Studio
+4. Import existing project -> Point to extracted folder
+5. Give it a minute to set everything up. Click **Install build tools and sync project** if you see it.
+
+### Bluemix Mobile Backend
+
+Next, you will need to setup a Bluemix Mobile Backend application which will handle sending push notifications to the Android application.
+
+1. Log into Bluemix
+2. **CREATE APP** -> **Mobile** -> \<PickABackendName\> -> **Finish**
+3. Click on **Dashboard** -> select your app -> **IBM Push Notifications** -> **Set up Push**
+4. Open ANOTHER tab: https://console.developers.google.com/
+5. **Select a Project** -> **Create a new project**
+6. Copy the **Project Number** from the top of the page, and Paste this under **Sender ID** in your Bluemix **Push Dashboard** Tab.
+7. Go back to the Google Developers Tab and click **APIs & auth -> APIs**
+8. **Cloud Messaging for Android -> Enable API**
+9. **Credentials -> Add Credentials -> API Key -> Server Key -> Create**. 
+10. Copy this key and and Paste this under **API Key** in your Bluemix **Push Dashbaord**
+
+### Connect Android Application to your Bluemix Mobile backend
+
+1. Click on **Project** in the top left corner and drill down to: `BlueBank/app/src/main/java/com/bluemix/bluebank/MainActivity.java`
+2. Around Line 50, fill in these two fields with your own values and **Save**
+```
+private String BluemixMobileBackendApplication_ROUTE = "http://backendURLxxxxxxx.mybluemix.net";
+private String BluemixMobileBackendApplication_App_GUID= "ef5xxxx-xxxx-xxxx-xxxx-xxxxxxxx";
+```
+   You can get both of these by clicking on **Mobile Options** in your Bluemix Mobile Backend application dashboard.
+
+### Feedback Manager application with Watson
+Next, you will need to deploy a node.js application which the Bank would use internally to manage all the feedback.
+
+1. Go here: https://github.com/IBM-Bluemix/HybridBanking-FeedbackManager
+2. If you have **git** installed, you know what to do. If not, click on **Download Zip** and extract it.
+3. Open **server.js** using your favorite editor and update these two lines below. You can find these values by clicking **Show Credentials** under IBM Push Notifications in your Bluemix Mobile Backend dashboard. This will allow this application to talk to the Bluemix mobile push service.
+```
+var IBMPushNotifications_url = "http://imfpush.ng.bluemix.net/imfpush/v1/apps/....";
+var IBMPushNotifications_appSecret  = "48xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+```
+Push this application to Bluemix. 
+```
+cd <location of your app>
+cf login -a https://api.ng.bluemix.net
+cf push <pickAUniqueAppName>
+```
+Bind the **Language Translation** and **Tone Analyzer** (in the Bluemix Labs catalog at the bottom of the regular catalog) services to this application. 
+
+### Link Android Application to the Feedback Manager app
+The Android application for this demo will submit the feedback directly to the node.js application we just created. In a real world scenario, the app would write to a database leveraging the Secure Gateway service and the Feedback Manager application would read from that database. You could maybe use APIm to expose that database as a Bluemix custom service. To keep things simple, we will skip this part.
+
+1. Open Android Studio again and drill down to: 
+`BlueBank/app/src/main/java/com/bluemix/bluebank/MainActivity.java`
+2. Around Line 55, update this variable with the route of the Feedback Manager application you created above.
+```    
+private String FeedbackApplicationRoute = "http://<FEEDBACKMANAGER>.mybluemix.net/";
+```
+**Save** and click the **Play** button at the top to launch your app in the emulator!
+
+
+### Demo
+1. Use the  Android application to submit feedback in English or Spanish.
+2. In your browser, visit your Feedback Manager application to see the new feedback and see the Watson analysis.
+3. Click on Award, type a message and click Send. The Android emulator should get a push notification!
---
+++ app/src/main/java/com/bluemix/bluebank/MainActivity.java
@@ -0,0 +1,229 @@
+package com.bluemix.bluebank;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPush;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPushException;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPushResponseListener;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    ////////////////////////////////////////////////
+    //FILL THESE OUT WITH YOUR OWN VALUES!
+    //Bluemix Mobile backend which will send push notifications
+
+    private String BluemixMobileBackendApplication_ROUTE = "http://YOURBACKENDROUTE.mybluemix.net";
+    private String BluemixMobileBackendApplication_App_GUID= "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx";
+
+    //Application which will receive the feedback submitted from this Android application.
+    private String FeedbackApplicationRoute = "http://FEEDBACKMANAGER_APP.mybluemix.net/";
+
+    //Your name
+    private String YourName = "Ram Vennam";
+    ////////////////////////////////////////////////
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    static DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
+    static Date date = new Date();
+
+    private MFPPush push = null;
+
+    //private static final String consumerID = "BlueBank_" + dateFormat.format(date);
+    private static final String consumerID = "BlueBankConsumer1";
+    private static final String deviceAlias = "TargetDevice";
+    EditText mEdit;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        try {
+            //Initialize the Core SDK
+            BMSClient.getInstance().initialize(getApplicationContext(), BluemixMobileBackendApplication_ROUTE, BluemixMobileBackendApplication_App_GUID);
+        } catch (MalformedURLException e) {
+            System.out.println("ERROR : Initialize the Core SDK");
+            e.printStackTrace();
+        }
+
+        //Initialize client Push SDK for Java
+        MFPPush.getInstance().initialize(getApplicationContext());
+        push = MFPPush.getInstance();
+
+
+        push.register(new MFPPushResponseListener<String>() {
+            @Override
+            public void onSuccess(String deviceId) {
+                System.out.println("Registration successful");
+            }
+
+            @Override
+            public void onFailure(MFPPushException ex) {
+                ex.printStackTrace();
+            }
+        });
+        final Activity activity = this;
+
+
+        final Button button = (Button) findViewById(R.id.button_submit);
+        mEdit = (EditText) findViewById(R.id.EditText_feedback);
+        mEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    button.callOnClick();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Thank you!")
+                        .setMessage("We received your feedback, and will get back to you shortly.")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                try {
+                                    new CallAPITask().execute(mEdit.getText().toString());
+                                    finish();
+                                    startActivity(getIntent());
+                                } catch (Exception e) {
+                                    Log.d(TAG, e.toString());
+                                }
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_info)
+                        .show();
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The
+
+
+        // action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Uses AsyncTask to create a task away from the main UI thread.
+    private class CallAPITask extends AsyncTask<String, Void, String> {
+
+        ListView list;
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                callAPI(params[0]);
+                return "Done!";
+            } catch (Exception e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(TAG, "onPost " + result);
+
+
+        }
+
+        public void callAPI(String feedback) throws Exception {
+            String feedbackAPIURL = Uri.parse(FeedbackApplicationRoute).buildUpon().appendPath("submitFeedback").toString();
+            URL url = new URL(feedbackAPIURL);
+            JSONObject payloadjson = new JSONObject();
+            payloadjson.put("user", YourName);
+            payloadjson.put("feedback", feedback);
+            Log.d(TAG, "feedback " + feedback);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
+
+            conn.setDoOutput(true);
+            OutputStream os = conn.getOutputStream();
+            os.write(payloadjson.toString().getBytes());
+            os.flush();
+
+            StringBuilder sb = new StringBuilder();
+            int HttpResult = conn.getResponseCode();
+            Log.d(TAG, String.valueOf(HttpResult));
+            if (HttpResult == HttpURLConnection.HTTP_OK) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    Log.d(TAG, line + "\n");
+                }
+
+                br.close();
+
+                Log.d(TAG, "" + sb.toString());
+
+            } else {
+                Log.d(TAG, conn.getResponseMessage());
+            }
+
+
+        }
+    }
+}
